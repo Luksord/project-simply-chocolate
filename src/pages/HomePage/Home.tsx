6d493190c6b77b40ee2f0f1bae566bd535b263ac
Łukasz Wątorski <watorski.info@gmail.com>
2025-04-23 00:01:02 +0200

    fundamentals-upload

---
+++ src/pages/HomePage/Home.tsx
@@ -0,0 +1,11 @@
+import css from './Home.module.css';
+
+export const Home = () => {
+  return (
+    <>
+      <div className={css.container}>
+        <h1 className={css.title}>HELLO WORLD</h1>
+      </div>
+    </>
+  );
+};
